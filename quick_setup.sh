50432ee24c888581ad35646c1c04a4b1454a9348
Wach <czet_88@hotmail.com>
2023-01-24 07:27:25 -0500

    Initial commit with first version of tutorial

---
+++ quick_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python -m venv my_env
+if [[ "$OSTYPE" == "msys" ]]; then
+	source ./my_env/Scripts/activate
+else 
+	source my_env/bin/activate
+fi
+pip install --upgrade pip
+pip install -r requirements.txt
+python -m ipykernel install --name=aaai_stats_tutorial
+jupyter notebook MaxFlow_Abstract.ipynb
+
